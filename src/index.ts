c5baa4d54aaaffcad38abbba863e5977b51c1769
Tomas Martinez <tomas.martinez@xmartlabs.com>
2022-11-17 09:15:36 -0300

    CORS package added

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express from "express";
 import { apiErrorHandler } from "./error/api-error-handler";
 import { router } from "./routes/router";
 import dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(apiErrorHandler);
