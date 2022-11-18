f608e698995ce4f388df7252320f3e30bef2d177
Tomas Martinez <tomas.martinez@xmartlabs.com>
2022-11-18 09:39:31 -0300

    Removed imports and variable declarations

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,15 +1,7 @@
 import { Request } from "express";
-import { PrismaClient, User } from "@prisma/client";
-import { HashManager } from "../utils/hash-manager";
-import { TokenManager } from "../utils/token-manager";
-import { ApiError } from "../error/api-error";
 import { UserService } from "../services/user-service";
 import { LoginResponseModel } from "../models/login-response-model";
 import { SignupResponseModel } from "../models/signup-response-model";
-
-const prisma = new PrismaClient();
-const hashManager = new HashManager();
-const tokenManager = new TokenManager();
 
 class UserController {
   userService: UserService;
@@ -17,13 +9,11 @@ class UserController {
     this.userService = new UserService();
   }
   async signup(req: Request): Promise<SignupResponseModel> {
-    const result = await this.userService.signup(req.body);
-    return result;
+    return await this.userService.signup(req.body);
   }
 
   async login(req: Request): Promise<LoginResponseModel> {
-    const result = await this.userService.login(req.body);
-    return result;
+    return await this.userService.login(req.body);
   }
 }
 
