93ee1ee55eff800392124d997154c5925bd8ead0
Tomas Martinez <tomas.martinez@xmartlabs.com>
2022-11-18 09:22:07 -0300

    Added more validations to dto's and messages

--- src/dtos/signup-dto.ts
+++ src/dtos/signup-dto.ts
@@ -1,8 +1,8 @@
 import * as yup from "yup";
 
 export default yup.object().shape({
-  firstname: yup.string().trim().required(),
-  lastname: yup.string().trim().required(),
-  email: yup.string().email().required(),
-  password: yup.string().required(),
+  firstname: yup.string().trim().required("Firstname is a required field").max(50, "Firstname can't contain more than 50 characters"),
+  lastname: yup.string().trim().required("Lastname is a required field").max(50, "Lastname can't contain more than 50 characters"),
+  email: yup.string().email().required("Email is a required field"),
+  password: yup.string().required("Password is a required field"),
 });
